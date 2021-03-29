1fa4ca9f29fa2383dd8dddf179a91a3504321eef
Edward Morgan <edwardwm@icloud.com>
2021-03-29 17:15:45 -0400

    Implemented + for Solution

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
 use std::fmt;
+use std::ops::Add;
+
+pub mod dictionary;
+pub mod grid;
 
 // Represents a solution for a Grid with a dictionary
-// TODO: we may want to implement Copy (and thus not use Vec)
 pub struct Solution {
   words_found: Vec<String>,
   score: u32
@@ -11,6 +14,11 @@ impl Solution {
    */
   pub fn new(words_found: Vec<String>, score: u32) -> Solution {
     Solution { words_found, score }
+  }
+
+  pub fn found(mut self, word: String, score: u32) -> () {
+    self.words_found.push(word);
+    self.score += score;
   }
 }
 impl fmt::Display for Solution {
@@ -24,4 +32,15 @@ impl fmt::Display for Solution {
       }
       write!(f, "")
     }
+}
+
+impl Add for Solution {
+  type Output = Self;
+  fn add(self, other: Self) -> Self {
+    let all_words = Vec::<String>::from([self.words_found.as_slice(), other.words_found.as_slice()].concat());
+    Self {
+      words_found: all_words,
+      score: self.score + other.score
+    }
+  }
 }
